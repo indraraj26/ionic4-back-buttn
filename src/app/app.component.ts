d0ae388e7bb6a80f69d2210a184bb1d3558c4871
Indraraj Yadav <indraraj@infiniteit.biz>
2019-10-27 13:30:06 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,26 @@
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Plugins, Capacitor } from '@capacitor/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
+  constructor(private platform: Platform) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      if (Capacitor.isPluginAvailable('SplashScreen')) {
+        Plugins.SplashScreen.hide();
+      }
+      if (Capacitor.isPluginAvailable('')) {
+        Plugins.StatusBar.setBackgroundColor({ color: '#ff0000' });
+      }
     });
   }
 }
